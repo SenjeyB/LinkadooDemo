cb4f41420c5d610069169e61b55701f90faf4c4d
SenjeyB <poltanov24@gmail.com>
2025-01-07 17:51:46 +0300

    UI/UX fixes

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,12 +1,13 @@
 CREATE TABLE IF NOT EXISTS users (
-                                     id SERIAL PRIMARY KEY,
-                                     username VARCHAR(255) NOT NULL UNIQUE,
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     nickname VARCHAR(255)
-    );
+);
 
 CREATE TABLE IF NOT EXISTS lobbies (
-                                       id BIGSERIAL PRIMARY KEY,
-                                       name VARCHAR(255) NOT NULL,
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
     creator_id BIGINT NOT NULL
-    );
+);
+    
